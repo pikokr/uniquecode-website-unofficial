c473be0e6bb314ed215b918d61525844055520f2
파랑이 <parangee9706@gmail.com>
2021-04-21 08:02:13 +0900

    feat: header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <DefaultHeader />
     <Nuxt />
   </div>
 </template>
+<script lang="ts">
+import DefaultHeader from '@/components/layouts/default/components/DefaultHeader.vue'
+export default {
+  components: { DefaultHeader },
+}
+</script>
