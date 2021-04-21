e72cf26e10fae9b0fffebc1f8486b6d9eb3d72fb
파랑이 <parangee9706@gmail.com>
2021-04-21 14:50:18 +0900

    feat: header background blur

--- components/layouts/default/components/DefaultHeader.vue
+++ components/layouts/default/components/DefaultHeader.vue
@@ -1,37 +1,21 @@
 <template>
   <header
-    class="py-6 ease-in-out sticky top-0 transition-colors"
-    :class="{ 'bg-white': !view.atTopOfPage }"
+    class="py-6 header-container ease-in-out bg-white sticky top-0 transition-colors bg-white bg-opacity-50"
   >
-    <div class="container mx-auto">test</div>
+    <div class="container px-4 mx-auto">test</div>
   </header>
 </template>
+
+<style scoped>
+.header-container {
+  backdrop-filter: blur(10px);
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   name: 'DefaultHeader',
-
-  data() {
-    return {
-      view: {
-        atTopOfPage: true,
-      },
-    }
-  },
-  beforeMount() {
-    window.addEventListener('scroll', this.handleScroll)
-  },
-
-  methods: {
-    handleScroll() {
-      if (window.pageYOffset > 0) {
-        if (this.view.atTopOfPage) this.view.atTopOfPage = false
-      } else {
-        !this.view.atTopOfPage && (this.view.atTopOfPage = true)
-      }
-    },
-  },
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,78 @@
 <template>
   <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi
-    architecto at beatae ducimus minus neque soluta temporibus. Asperiores dolor
-    dolore eligendi eos eveniet harum iste quam quia reprehenderit voluptates.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque
+    facere modi quam quo. Accusantium architecto autem commodi, ducimus eum fuga
+    laboriosam maiores odio quam quisquam similique veritatis vero, voluptate!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae dolorum
+    eius eligendi, expedita, nihil numquam perferendis provident sapiente
+    voluptas voluptate voluptatem voluptates? Culpa dignissimos, eius laborum
+    perspiciatis quia ut. Lorem ipsum dolor sit amet, consectetur adipisicing
+    elit. Ad at culpa cumque dicta dolore, dolores eaque earum error impedit
+    molestiae necessitatibus neque odio qui quia quis recusandae, saepe tempore
+    voluptate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+    culpa distinctio iste magni quos voluptate voluptatem? Ad asperiores earum
+    ipsum mollitia officiis quam quis ratione rem sapiente, sunt velit
+    voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Aliquid cumque facere modi quam quo. Accusantium architecto autem commodi,
+    ducimus eum fuga laboriosam maiores odio quam quisquam similique veritatis
+    vero, voluptate! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    At beatae dolorum eius eligendi, expedita, nihil numquam perferendis
+    provident sapiente voluptas voluptate voluptatem voluptates? Culpa
+    dignissimos, eius laborum perspiciatis quia ut. Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Ad at culpa cumque dicta dolore, dolores eaque
+    earum error impedit molestiae necessitatibus neque odio qui quia quis
+    recusandae, saepe tempore voluptate! Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Autem culpa distinctio iste magni quos voluptate
+    voluptatem? Ad asperiores earum ipsum mollitia officiis quam quis ratione
+    rem sapiente, sunt velit voluptates! Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Aliquid cumque facere modi quam quo. Accusantium
+    architecto autem commodi, ducimus eum fuga laboriosam maiores odio quam
+    quisquam similique veritatis vero, voluptate! Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. At beatae dolorum eius eligendi, expedita,
+    nihil numquam perferendis provident sapiente voluptas voluptate voluptatem
+    voluptates? Culpa dignissimos, eius laborum perspiciatis quia ut. Lorem
+    ipsum dolor sit amet, consectetur adipisicing elit. Ad at culpa cumque dicta
+    dolore, dolores eaque earum error impedit molestiae necessitatibus neque
+    odio qui quia quis recusandae, saepe tempore voluptate! Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. Autem culpa distinctio iste magni
+    quos voluptate voluptatem? Ad asperiores earum ipsum mollitia officiis quam
+    quis ratione rem sapiente, sunt velit voluptates! Lorem ipsum dolor sit
+    amet, consectetur adipisicing elit. Aliquid cumque facere modi quam quo.
+    Accusantium architecto autem commodi, ducimus eum fuga laboriosam maiores
+    odio quam quisquam similique veritatis vero, voluptate! Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. At beatae dolorum eius eligendi,
+    expedita, nihil numquam perferendis provident sapiente voluptas voluptate
+    voluptatem voluptates? Culpa dignissimos, eius laborum perspiciatis quia ut.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at culpa cumque
+    dicta dolore, dolores eaque earum error impedit molestiae necessitatibus
+    neque odio qui quia quis recusandae, saepe tempore voluptate! Lorem ipsum
+    dolor sit amet, consectetur adipisicing elit. Autem culpa distinctio iste
+    magni quos voluptate voluptatem? Ad asperiores earum ipsum mollitia officiis
+    quam quis ratione rem sapiente, sunt velit voluptates! Lorem ipsum dolor sit
+    amet, consectetur adipisicing elit. Aliquid cumque facere modi quam quo.
+    Accusantium architecto autem commodi, ducimus eum fuga laboriosam maiores
+    odio quam quisquam similique veritatis vero, voluptate! Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. At beatae dolorum eius eligendi,
+    expedita, nihil numquam perferendis provident sapiente voluptas voluptate
+    voluptatem voluptates? Culpa dignissimos, eius laborum perspiciatis quia ut.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at culpa cumque
+    dicta dolore, dolores eaque earum error impedit molestiae necessitatibus
+    neque odio qui quia quis recusandae, saepe tempore voluptate! Lorem ipsum
+    dolor sit amet, consectetur adipisicing elit. Autem culpa distinctio iste
+    magni quos voluptate voluptatem? Ad asperiores earum ipsum mollitia officiis
+    quam quis ratione rem sapiente, sunt velit voluptates! Lorem ipsum dolor sit
+    amet, consectetur adipisicing elit. Aliquid cumque facere modi quam quo.
+    Accusantium architecto autem commodi, ducimus eum fuga laboriosam maiores
+    odio quam quisquam similique veritatis vero, voluptate! Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. At beatae dolorum eius eligendi,
+    expedita, nihil numquam perferendis provident sapiente voluptas voluptate
+    voluptatem voluptates? Culpa dignissimos, eius laborum perspiciatis quia ut.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at culpa cumque
+    dicta dolore, dolores eaque earum error impedit molestiae necessitatibus
+    neque odio qui quia quis recusandae, saepe tempore voluptate! Lorem ipsum
+    dolor sit amet, consectetur adipisicing elit. Autem culpa distinctio iste
+    magni quos voluptate voluptatem? Ad asperiores earum ipsum mollitia officiis
+    quam quis ratione rem sapiente, sunt velit voluptates!
   </div>
 </template>
 
