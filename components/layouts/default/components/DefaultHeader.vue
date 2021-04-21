a2ef35378a556af62548f1dfb9c71cc0a30d13ab
파랑이 <parangee9706@gmail.com>
2021-04-21 15:21:30 +0900

    feat: use nuxt link on header

--- components/layouts/default/components/DefaultHeader.vue
+++ components/layouts/default/components/DefaultHeader.vue
@@ -3,14 +3,14 @@
     class="py-6 header-container ease-in-out bg-white sticky top-0 transition-colors bg-white bg-opacity-50"
   >
     <div class="container px-6 mx-auto">
-      <div class="flex gap-4 items-center">
+      <nuxt-link to="/" class="flex gap-4 items-center">
         <img
           :src="require('@/assets/img/logo.png')"
           class="w-12 h-12"
           alt="Logo"
         />
         <span class="text-3xl font-bold">Uniquecode</span>
-      </div>
+      </nuxt-link>
     </div>
   </header>
 </template>
