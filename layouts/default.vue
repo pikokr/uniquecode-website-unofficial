a792e1c14a4f5c09425e758150a692b50dac2026
파랑이 <parangee9706@gmail.com>
2021-04-28 16:16:11 +0900

    feat: include footer in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,13 @@
   <div>
     <DefaultHeader />
     <Nuxt />
+    <DefaultFooter />
   </div>
 </template>
 <script lang="ts">
 import DefaultHeader from '@/components/layouts/default/components/DefaultHeader.vue'
+import DefaultFooter from '~/components/layouts/default/components/DefaultFooter.vue'
 export default {
-  components: { DefaultHeader },
+  components: { DefaultFooter, DefaultHeader },
 }
 </script>
