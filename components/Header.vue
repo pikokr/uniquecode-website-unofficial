57342235f87cac54edb65d7fc229c800544702e8
pikokr <parangee9706@gmail.com>
2021-08-04 14:50:48 +0900

    feat: header links area

--- components/Header.vue
+++ components/Header.vue
@@ -5,6 +5,11 @@
         <div class="logo" />
         <span class="logo-text">Uniquecode</span>
       </nuxt-link>
+      <div class="links-area">
+        <nuxt-link to="/projects">프로젝트</nuxt-link>
+        <nuxt-link to="/members">멤버</nuxt-link>
+        <nuxt-link to="/recruit">채용</nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -26,7 +31,17 @@ export default {
     max-width: 1000px;
     width: 100%;
     display: flex;
+    align-items: center;
     justify-content: space-between;
+    .links-area {
+      display: flex;
+      gap: 50px;
+      a {
+        text-decoration: none;
+        color: #000;
+        font-size: 25px;
+      }
+    }
     .logo-area {
       display: flex;
       gap: 20px;
