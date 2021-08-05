6d539b03be050725880a5e46a3efe2d1135801af
pikokr <parangee9706@gmail.com>
2021-08-05 09:46:35 +0900

    feat: hero section

--- components/views/Home.vue
+++ components/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <div class="hero">asdf</div>
+        <div class="hero">
+            <div class="content">
+                <div class="text-area">
+                    <div class="text">
+                        당신의 디스코드에<br />유니크한 재미를 더.
+                    </div>
+                </div>
+                <div class="discord-logo" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,5 +29,27 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+
+    .content {
+        display: flex;
+        justify-content: space-between;
+        gap: 75px;
+        max-width: 1150px;
+        width: 100%;
+        .discord-logo {
+            width: 305px;
+            height: 350px;
+            background-image: url('~/assets/views/home/svg/discord.svg');
+        }
+        .text-area {
+            display: flex;
+            flex-direction: column;
+            .text {
+                font-weight: 700;
+                font-size: 90px;
+                opacity: 0.7;
+            }
+        }
+    }
 }
 </style>
